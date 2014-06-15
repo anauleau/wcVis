b41cc16de39b79b7075d4d4bd0272c2805d0939c
Andreas Nauleau <andreas@adap.tv>
2014-06-15 09:58:56 -0700

    adds summary page with basic stats

--- scripts/controllers/summaryCtrl.js
+++ scripts/controllers/summaryCtrl.js
@@ -1,4 +1,24 @@
 worldCupApp.controller('summaryCtrl', ['$scope', 'dataService',
     function ($scope, dataService) {
         $scope.name = "Summary";
+        $scope.totalWorldCupTeams = 0;
+        $scope.totalWorldCups = 0;
+        $scope.totalGoalsScored = 0;
+        $scope.totalMatchesPlayed = 0;
+        $scope.totalAttendence = 0;
+
+        dataService.getTeams().then(function (teams) {
+            angular.forEach(teams, function () {
+                $scope.totalWorldCupTeams ++;
+            });
+        });
+
+        dataService.getTournaments().then(function (tournaments) {
+            angular.forEach(tournaments, function (value) {
+                $scope.totalWorldCups ++;
+                $scope.totalMatchesPlayed += value.summary.matchesPlayed;
+                $scope.totalGoalsScored += value.summary.totalGoals;
+                $scope.totalAttendence += value.summary.attendance;
+            });
+        });
     }]);
