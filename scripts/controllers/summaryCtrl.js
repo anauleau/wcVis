5870bfc29b387eb4a48a7ed5def819508427bccf
Andreas Nauleau <andreas@adap.tv>
2014-06-17 22:12:04 -0700

    fixes loading issue

--- scripts/controllers/summaryCtrl.js
+++ scripts/controllers/summaryCtrl.js
@@ -1,23 +1,29 @@
 worldCupApp.controller('summaryCtrl', ['$scope', 'dataService', '$filter',
     function ($scope, dataService) {
         // feature summary stats and charts
+        var totalTeams = 0,
+            totalMatches = 0,
+            totalWorldCups = 0,
+            totalGoals = 0,
+            totalAttendence = 0;
         $scope.name = "Summary";
-        $scope.totalWorldCupTeams = 0;
-        $scope.totalWorldCups = 0;
-        $scope.totalGoalsScored = 0;
-        $scope.totalMatchesPlayed = 0;
-        $scope.totalAttendence = 0;
 
         angular.forEach($scope.data.teams, function () {
-            $scope.totalWorldCupTeams ++;
+            totalTeams ++;
         });
 
         angular.forEach($scope.data.tournaments, function (value) {
-            $scope.totalWorldCups ++;
-            $scope.totalMatchesPlayed += value.summary.matchesPlayed;
-            $scope.totalGoalsScored += value.summary.totalGoals;
-            $scope.totalAttendence += value.summary.attendance;
+            totalWorldCups ++;
+            totalMatches += value.summary.matchesPlayed;
+            totalGoals += value.summary.totalGoals;
+            totalAttendence += value.summary.attendance;
         });
+
+        $scope.totalWorldCupTeams = totalTeams;
+        $scope.totalWorldCups = totalWorldCups;
+        $scope.totalGoalsScored = totalGoals;
+        $scope.totalMatchesPlayed = totalMatches;
+        $scope.totalAttendence = totalAttendence;
 
         $scope.goalsPerTournament = [
             {
