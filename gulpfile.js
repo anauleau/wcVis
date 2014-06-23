d95cbb293abe674e6df7b35907fa69a0a2891388
Andreas Nauleau <andreas@adap.tv>
2014-06-23 10:54:30 -0700

    adds .gitignore file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
-var gulp = require('gulp');
-
-var htmlify = require('gulp-angular-htmlify');
+var gulp = require('gulp'),
+    htmlify = require('gulp-angular-htmlify');
 
 gulp.task('htmlify', function() {
     gulp.src('views/*.html')
