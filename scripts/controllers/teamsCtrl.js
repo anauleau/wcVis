14aaa9ab4e836a716b5afb64e2d66b7f2d603c2b
Andreas Nauleau <andreas@adap.tv>
2014-06-24 13:46:53 -0700

    fixes img not found bug when model not selected

--- scripts/controllers/teamsCtrl.js
+++ scripts/controllers/teamsCtrl.js
@@ -1,5 +1,7 @@
 worldCupApp.controller('teamsCtrl', ['$scope', 'sharedData',
     function ($scope, sharedData) {
         // attach teams from shared data service to scope
-        $scope.teams = sharedData.teams;
+        sharedData.getTeams().then(function() {
+            $scope.teams = sharedData.teams;
+        });
     }]);
--- scripts/controllers/tournamentsCtrl.js
+++ scripts/controllers/tournamentsCtrl.js
@@ -1,5 +1,7 @@
 worldCupApp.controller('tournamentsCtrl', ['$scope', 'sharedData',
     function ($scope, sharedData) {
         // attach tournamnets from sharedData service to scope
-        $scope.tournaments = sharedData.tournaments;
+        sharedData.getTournaments().then(function() {
+            $scope.tournaments = sharedData.tournaments;
+        });
     }]);
